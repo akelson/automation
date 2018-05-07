446bd78ca0c8722243c8c3969a5fa08057f1cf27
Adam Kelson <akelson@nerdypage.com>
2018-05-06 23:01:35 -0600

    Fix typo.

--- lights/schedlights.sh
+++ lights/schedlights.sh
@@ -5,8 +5,8 @@ DIR=`dirname "${BASH_SOURCE}"`
 # The time 30 min before sunset.
 TIME_ON=`${DIR}/sunset.py --offsetmins=-30`
 
-# Turn the lights on.
-echo "${DIR}/cmd_ifttt_event.sh lights_on_living_room" | at ${TIME_ON}
+# Turn the light on.
+echo "${DIR}/cmd_ifttt_event.sh light_on_living_room" | at ${TIME_ON}
 
-# Turn the lights off.
-echo "${DIR}/cmd_ifttt_event.sh lights_off_living_room" | at 22:30
+# Turn the light off.
+echo "${DIR}/cmd_ifttt_event.sh light_off_living_room" | at 22:30
