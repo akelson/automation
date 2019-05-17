f3de57d3e29a8d62cf6d35ef8064fd3ad3a4e538
Adam Kelson <adam@adamkelson.com>
2019-05-17 15:27:06 -0600

    Add logging.

--- moosefood5k/feed.py
+++ moosefood5k/feed.py
@@ -2,6 +2,7 @@ import argparse
 import requests
 import json
 import os
+import logging
 
 def getConfig():
     config_path = os.path.expanduser('~/moosefood.json')
@@ -13,15 +14,23 @@ def feed(grams):
     url = config['url']
     grams_per_sec = float(config['grams_per_min']) / 60
     feed_dur_sec = str(int(grams / grams_per_sec))
-    print feed_dur_sec
-    r = requests.get(url + '/feed', params = {'seconds' : feed_dur_sec})
-    print r
-    print r.content
 
-if '__main__' == __name__:
+    logging.info("Feeding {} grams in {} sec.".format(grams, feed_dur_sec))
+    r = requests.get(url + '/feed', params = {'seconds' : feed_dur_sec})
+    logging.info("status_code: {}".format(r.status_code))
+
+def main():
+    logging.basicConfig(
+        filename = 'moosefood.log', 
+        level = logging.INFO,
+        format = '%(asctime)s %(levelname)s: %(message)s')
+
     parser = argparse.ArgumentParser(description = 'Feed the Moose.')
-    parser.add_argument('grams', type = float, help = 'Dispense food.')
+    parser.add_argument('grams', type = float, help = 'Grams of food to dispense..')
 
     args = parser.parse_args();
 
     feed(args.grams)
+
+if '__main__' == __name__:
+    main()
